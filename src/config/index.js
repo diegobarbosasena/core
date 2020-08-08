83ba5c282b9abe00f03a4e9c03570944e0cbe405
Diego Barbosa Sena <diegosena_93@hotmail.com>
2020-08-08 20:50:12 -0300

    ajuste na URL do backend

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://core-imersao-react-alura.herokuapp.com/'
+  : 'https://core-imersao-react-alura.herokuapp.com'
 
 export default {
   URL_BACKEND,
